7871baa70dc2307820850a4ed32302e4315aff9b
kkoma <lijinhang460@gmail.com>
2025-09-11 01:25:55 +0900

    项目后端初始化

---
+++ src/main/java/com/example/codemother/controller/HealthController.java
@@ -0,0 +1,19 @@
+package com.example.codemother.controller;
+
+
+import com.example.codemother.common.BaseResponse;
+import com.example.codemother.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/")
+    public BaseResponse<String> healthCheck() {
+        return ResultUtils.success( "ok");
+    }
+}
+
---
+++ src/test/java/com/example/codemother/CodeMotherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codemother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeMotherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
