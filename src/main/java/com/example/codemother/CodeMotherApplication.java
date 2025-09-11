03ddb1f7ed8935aa1aaf5a14740802aa1a1ff9c9
kkoma <lijinhang460@gmail.com>
2025-09-12 00:54:07 +0900

    后端基础内容开发

--- src/main/java/com/example/codemother/CodeMotherApplication.java
+++ src/main/java/com/example/codemother/CodeMotherApplication.java
@@ -1,9 +1,12 @@
 package com.example.codemother;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//添加扫描文件
+@MapperScan("com.example.codemother.mapper")
 public class CodeMotherApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/codemother/model/dto/UserLogging.java
@@ -0,0 +1,24 @@
+package com.example.codemother.model.dto;
+
+import lombok.Data;
+import java.io.Serializable;
+
+@Data
+public class UserLogging implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 账号
+     */
+    private String userAccount;
+
+    /**
+     * 密码
+     */
+    private String userPassword;
+
+    /**
+     * 确认密码
+     */
+    private String checkPassword;
+}
