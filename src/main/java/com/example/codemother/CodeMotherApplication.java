33f944b3f4b6401d63e45a24c6477c486eb671e5
kkoma <lijinhang460@gmail.com>
2025-09-15 02:43:19 +0900

    持续开发，功能完善

--- src/main/java/com/example/codemother/CodeMotherApplication.java
+++ src/main/java/com/example/codemother/CodeMotherApplication.java
@@ -3,10 +3,12 @@ package com.example.codemother;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 //添加扫描文件
 @MapperScan("com.example.codemother.mapper")
+@ComponentScan({"com.example.codemother", "core"})
 public class CodeMotherApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/codemother/ai/AiCodeGeneratorService.java
+++ src/main/java/com/example/codemother/ai/AiCodeGeneratorService.java
@@ -1,5 +1,7 @@
 package com.example.codemother.ai;
 
+import com.example.codemother.ai.model.HtmlCodeResult;
+import com.example.codemother.ai.model.MultiFileCodeResult;
 import dev.langchain4j.service.SystemMessage;
 
 public interface AiCodeGeneratorService {
@@ -10,8 +12,26 @@ public interface AiCodeGeneratorService {
      * @param userMessage 用户消息
      * @return 生成的代码结果
      */
+//    @SystemMessage(fromResource = "prompt/codegen-html-system-prompt.txt")
+//    String generateHtmlCode(String userMessage);
+
+    /**
+     * 生成多文件代码
+     *
+     * @param userMessage 用户消息
+     * @return 生成的代码结果
+     */
+//    @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
+//    String generateMultiFileCode(String userMessage);
+
+    /**
+     * 生成 HTML 代码
+     *
+     * @param userMessage 用户消息
+     * @return 生成的代码结果
+     */
     @SystemMessage(fromResource = "prompt/codegen-html-system-prompt.txt")
-    String generateHtmlCode(String userMessage);
+    HtmlCodeResult generateHtmlCode(String userMessage);
 
     /**
      * 生成多文件代码
@@ -20,5 +40,6 @@ public interface AiCodeGeneratorService {
      * @return 生成的代码结果
      */
     @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
-    String generateMultiFileCode(String userMessage);
+    MultiFileCodeResult generateMultiFileCode(String userMessage);
+
 }
