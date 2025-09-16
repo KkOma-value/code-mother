80796c8aecd7b8620d6441d9d0352f8d4825e315
kkoma <lijinhang460@gmail.com>
2025-09-16 17:30:29 +0800

    AI应用开发 1.2

---
+++ src/main/java/com/example/codemother/mapper/AppMapper.java
@@ -0,0 +1,14 @@
+package com.example.codemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.example.codemother.model.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ * @author kkoma
+ * @since 1.1
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
---
+++ src/main/java/com/example/codemother/service/AppService.java
@@ -0,0 +1,28 @@
+package com.example.codemother.service;
+
+import com.example.codemother.model.dto.App.AppQueryRequest;
+import com.example.codemother.model.entity.User;
+import com.example.codemother.model.vo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import com.example.codemother.model.entity.App;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+/**
+ * 应用 服务层。
+ *
+ * @author kkoma
+ * @since 1.1
+ */
+public interface AppService extends IService<App> {
+
+    AppVO getAppVO(App app);
+
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+    List<AppVO> getAppVOList(List<App> appList);
+
+    Flux<String> chatToGenCode(Long appId, String message, User loginUser);
+}
