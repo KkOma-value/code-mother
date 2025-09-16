ec54c9ff5b2dac96ec746fa16bf926aec8eed857
kkoma <lijinhang460@gmail.com>
2025-09-16 17:17:00 +0800

    AI应用开发

--- src/main/java/com/example/codemother/generator/MyBatisCodeGenerator.java
+++ src/main/java/com/example/codemother/generator/MyBatisCodeGenerator.java
@@ -12,7 +12,7 @@ import java.util.Map;
 public class MyBatisCodeGenerator {
 
     // 需要生成的表名
-    private static final String[] TABLE_NAMES = {"user"};
+    private static final String[] TABLE_NAMES = {"app"};
 
     public static void main(String[] args) {
         // 获取数据源信息
@@ -71,7 +71,7 @@ public class MyBatisCodeGenerator {
         // 设置生成时间和字符串为空，避免多余的代码改动
         globalConfig.getJavadocConfig()
                 .setAuthor("kkoma")
-                .setSince("1.0");
+                .setSince("1.1");
         return globalConfig;
     }
 }
