a63ae2e9e066e537160377b35684dd1bc527fc0a
kkoma <lijinhang460@gmail.com>
2025-09-28 03:17:17 +0900

    9-28 大模型记忆开发

---
+++ src/main/java/com/example/codemother/controller/StaticResourceController.java
@@ -0,0 +1,9 @@
+package com.example.codemother.controller;
+
+import com.example.codemother.constant.AppConstant;
+
+public class StaticResourceController {
+    // 应用生成根目录（用于浏览）
+    private static final String PREVIEW_ROOT_DIR = AppConstant.CODE_OUTPUT_ROOT_DIR;
+
+}
